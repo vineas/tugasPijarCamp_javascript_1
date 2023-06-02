dbf9c70c1f10167d6b249be44314c6dc8eba97f2
vineas <alvienasyandika@gmail.com>
2023-06-02 20:30:01 +0700

    tugas nomer 1 tapi schoolist belum

---
+++ tugas.js
@@ -0,0 +1,33 @@
+// nomer 1
+const biodata = {
+    name : "Alvienas Yandika",
+    age : 24,
+    hobbies :["olahraga", "makan", "game"],
+    isMarried : false,
+    schoolList :[
+        {
+            name: "",
+            yearIn: "",
+            yearOut: "",
+            major: ""
+        },
+        {
+            name: "",
+            yearIn: "",
+            yearOut: "",
+            major: ""
+        },
+        {
+            name: "",
+            yearIn: "",
+            yearOut: "",
+            major: ""
+        },
+        {
+            name: "",
+            yearIn: "",
+            yearOut: "",
+            major: ""
+        }
+    ]
+}
